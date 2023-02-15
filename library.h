5b7282e5ef7a4832e08e11ce3a59e9f483819ce4
akshaykuma2 <100851136+akshaykuma2@users.noreply.github.com>
2023-02-15 14:44:18 +0530

    Update library.h

--- library.h
+++ library.h
@@ -2,8 +2,8 @@ char* strchr1(const char* p1, char c);
 char* stringncopy(char* p1,char* p2,int len);
 char* revrse(char* p1,int len);
 char* stringcopy(char* p1,char* p2);
-char* strtok1(char* str, const char* delim);
 char* strcon1(char* p1,char* p2);
+void *memcpy1(void *p2,void *p1,int n)
 
 
 
